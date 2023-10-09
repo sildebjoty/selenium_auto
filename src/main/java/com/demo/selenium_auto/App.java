3efd6ea37b7ab9ce957c15f02bc0f9739d09b7c7
Debjoty Sil <sil_debjoty@yahoo.com>
2023-10-08 22:34:26 -0500

    First Commit

---
+++ src/main/java/com/demo/selenium_auto/App.java
@@ -0,0 +1,51 @@
+package com.demo.selenium_auto;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // Set the path to the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "/Users/debjotysil/Documents/software/ChromeDriver.app");
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        
+        // Initialize ChromeDriver
+        WebDriver driver = new ChromeDriver(chromeOptions);
+
+        // Navigate to Facebook
+        driver.get("https://www.facebook.com");
+
+        // Find the email/phone input field and enter your email/phone
+        WebElement emailInput = driver.findElement(By.id("email"));
+        emailInput.sendKeys("abc@yahoo.com");
+
+        // Find the password input field and enter your password
+        WebElement passwordInput = driver.findElement(By.id("pass"));
+        passwordInput.sendKeys("abc");
+
+        // Find the login button and click it
+        WebElement loginButton = driver.findElement(By.name("login"));
+        loginButton.click();
+
+        // Sleep for a few seconds to see the result (You should use explicit waits instead)
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Close the browser
+        driver.quit();
+
+    }
+}
